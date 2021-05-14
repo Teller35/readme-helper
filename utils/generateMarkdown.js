9b23b13774f44da70c5c896c3c19fda3ad5d299d
Teller Wetzel <tellerwetzel@yahoo.com>
2021-05-14 16:11:01 -0600

    added license section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,15 +1,30 @@
 const fs = require('fs');
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return `[![License Badge](https://img.shields.io/badge/license-${license}-blue.svg)](#license)`
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return `[![License Link](https://choosealicense.com/licenses/-${license})](#license)`
+  }
+  return `There is no license to link!`
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return `This project is covered under the ${license} license!`
+  }
+  return `This project does not have any license!`
+}
 
 // TODO: Create a function to generate markdown for README
 const generateInstall = installText => {
@@ -47,6 +62,7 @@ const generateTest = testText => {
 
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
 
   ## Description:
   #### *${data.description}*
@@ -71,7 +87,7 @@ function generateMarkdown(data) {
   ${generateCredit(data.credits)}
 
   ## License
-  #### *${data.license}*
+  #### *${renderLicenseSection(data.license)}*
 
   ## Contribute
   ${generateCont(data.contribute)}
